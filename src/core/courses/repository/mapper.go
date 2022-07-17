f9ccb906d812f2534974e3d16391a40e7a14e290
Jean <jean.carlosmolossi@gmail.com>
2022-07-16 23:13:43 -0300

    fix: if model nil or times nil, validate

--- src/core/courses/repository/mapper.go
+++ src/core/courses/repository/mapper.go
@@ -8,6 +8,10 @@ import (
 
 // ModelToDomain converts a CourseModel to a CourseDomain.
 func ModelToDomain(model *CourseModel) domain.Course {
+	if model == nil {
+		return domain.NewCourse("", "", "", false)
+	}
+
 	domainCourse := domain.NewCourse(
 		model.CourseTitle,
 		model.CourseThumb,
@@ -21,13 +25,25 @@ func ModelToDomain(model *CourseModel) domain.Course {
 
 // DomainToModel converts a CourseDomain to a CourseModel.
 func DomainToModel(course domain.Course, createdAt, updatedAt *time.Time) *CourseModel {
+	var createdAtVal, updatedAtVal time.Time
+
+	// If createdAt is not nil, set it to the current time.
+	if createdAt != nil {
+		createdAtVal = *createdAt
+	}
+
+	// If updatedAt is not nil, set it to the current time.
+	if updatedAt != nil {
+		updatedAtVal = *updatedAt
+	}
+
 	return &CourseModel{
 		CourseID:          course.GetCourseID(),
 		CourseTitle:       course.GetCourseTitle(),
 		CourseThumb:       course.GetCourseThumb(),
 		CourseDescription: course.GetCourseDesc(),
 		CoursePublished:   course.IsCoursePublished(),
-		CourseCreatedAt:   *createdAt,
-		CourseUpdatedAt:   *updatedAt,
+		CourseCreatedAt:   createdAtVal,
+		CourseUpdatedAt:   updatedAtVal,
 	}
 }
