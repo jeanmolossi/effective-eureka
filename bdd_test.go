a62f877dca754b45fc4faf28c13c4ca8bb05967d
Jean <jean.carlosmolossi@gmail.com>
2022-07-24 15:45:56 -0300

    feat(bdd_test): added given step

--- bdd_test.go
+++ bdd_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -12,11 +13,74 @@ import (
 	"strings"
 
 	"github.com/cucumber/godog"
+	"github.com/jeanmolossi/effective-eureka/src/core/shared"
 )
 
 type apiFeature struct {
 	baseURL string
 	resp    *httptest.ResponseRecorder
+}
+
+var token string
+
+func DoRegister() error {
+	baseURL := os.Getenv("API_HOST")
+	registerEndpoint := fmt.Sprintf("%s%s", baseURL, "/students/register")
+
+	resp, err := http.Post(registerEndpoint, "application/json",
+		strings.NewReader(`{"password": "123456789","username": "john@doe.com"}`),
+	)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 201 {
+		bytes, _ := io.ReadAll(resp.Body)
+		if !strings.Contains(string(bytes), "already exists") {
+			return errors.New("failed to register student")
+		}
+	}
+
+	return nil
+}
+
+func DoLogin() error {
+	baseURL := os.Getenv("API_HOST")
+	loginEndpoint := fmt.Sprintf("%s%s", baseURL, "/auth/login")
+
+	resp, err := http.Post(loginEndpoint, "application/json",
+		strings.NewReader(`{"password": "123456789","username": "john@doe.com"}`),
+	)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New("failed to login student")
+	}
+
+	response, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	type body struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	var b body
+	err = json.Unmarshal(response, &b)
+	if err != nil {
+		return err
+	}
+
+	token = b.AccessToken
+
+	return nil
+}
+
+func addTokenToRequest(req *http.Request) {
+	req.Header.Add("Authorization", token)
 }
 
 func (a *apiFeature) resetResponse(*godog.Scenario) {
@@ -31,6 +95,8 @@ func (a *apiFeature) iRequestTo(method, endpoint string) (err error) {
 	if err != nil {
 		return
 	}
+
+	addTokenToRequest(req)
 
 	defer func() {
 		switch t := recover().(type) {
@@ -76,6 +142,7 @@ func (a *apiFeature) iRequestToWithPayload(method, endpoint string, payload *god
 		return
 	}
 
+	addTokenToRequest(req)
 	req.Header.Add("Content-Type", "application/json")
 
 	defer func() {
@@ -142,9 +209,56 @@ func (a *apiFeature) theResponseMatchJSON(body *godog.DocString) (err error) {
 	return nil
 }
 
+func (a *apiFeature) thereAreAny(tableName string, data *godog.Table) error {
+	var fields []string
+	var marks []string
+
+	head := data.Rows[0].Cells
+	for _, cell := range head {
+		fields = append(fields, cell.Value)
+		marks = append(marks, "?")
+	}
+
+	dbConn := shared.NewDbConnection()
+	err := dbConn.Connect()
+	if err != nil {
+		return err
+	}
+
+	for i := 1; i < len(data.Rows); i++ {
+		var vals []interface{}
+		for _, cell := range data.Rows[i].Cells {
+			vals = append(vals, cell.Value)
+		}
+
+		stmt := dbConn.DB().Exec(
+			`INSERT INTO `+tableName+` (`+strings.Join(fields, ",")+`) VALUES (`+strings.Join(marks, ",")+`)`,
+			vals...,
+		)
+
+		if err := stmt.Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	api := &apiFeature{
 		baseURL: os.Getenv("API_HOST"),
+	}
+
+	err := DoRegister()
+	if err != nil {
+		panic(err)
+	}
+
+	// if has no token should login
+	if token == "" {
+		if err := DoLogin(); err != nil {
+			panic(err)
+		}
 	}
 
 	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
@@ -156,5 +270,5 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^I "(POST|PUT)" to "([^"]*)" with:$`, api.iRequestToWithPayload)
 	ctx.Step(`^the status code received should be (\d+)$`, api.theStatusCodeShouldBe)
 	ctx.Step(`^the response received should match json:$`, api.theResponseMatchJSON)
-
+	ctx.Step(`^there are "([^"]*)" with:$`, api.thereAreAny)
 }
